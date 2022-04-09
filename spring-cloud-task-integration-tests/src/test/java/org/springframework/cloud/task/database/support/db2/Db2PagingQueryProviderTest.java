8bd52da6998ca4379f372b9407eae6d1b35d5d38
Tyler Carpenter-Rivers <tyler2cr@gmail.com>
2022-04-09 03:12:48 -0700

    checkstyle

--- spring-cloud-task-integration-tests/src/test/java/org/springframework/cloud/task/database/support/db2/Db2PagingQueryProviderTest.java
+++ spring-cloud-task-integration-tests/src/test/java/org/springframework/cloud/task/database/support/db2/Db2PagingQueryProviderTest.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2015-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.task.database.support.db2;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +39,8 @@ import org.testcontainers.containers.Db2Container;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
 import java.util.Date;
@@ -32,10 +48,10 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.context.jdbc.SqlConfig.ErrorMode.CONTINUE_ON_ERROR;
 
 /**
+ * @author Tyler Carpenter-Rivers
  * @see Db2PagingQueryProvider
  */
 @SpringBootTest(
@@ -64,26 +80,8 @@ class Db2PagingQueryProviderTest {
 		registry.add("spring.datasource.password", db2Container::getPassword);
 	}
 
-	@TestConfiguration
-	public static class TestConfig {
-		@Bean
-		@ConfigurationProperties("spring.datasource")
-		public DataSource dataSource() {
-			return new DriverManagerDataSource();
-		}
-		@Bean
-		public TaskExecutionDaoFactoryBean taskExecutionDaoFactoryBean(DataSource dataSource) {
-			return new TaskExecutionDaoFactoryBean(dataSource);
-		}
-		@Bean
-		public TaskExecutionDao taskExecutionDao(TaskExecutionDaoFactoryBean taskExecutionDaoFactoryBean) throws Exception {
-			return taskExecutionDaoFactoryBean.getObject();
-		}
-	}
-
 	@Autowired
 	private SimpleTaskExplorer simpleTaskExplorer;
-
 
 	@Autowired
 	private TaskExecutionDao taskExecutionDao;
@@ -113,7 +111,28 @@ class Db2PagingQueryProviderTest {
 			.as("Expected the total number of elements to exceed the page request size")
 			.isGreaterThan(pageRequest.getPageSize());
 
-		assertEquals(pageRequest.getPageSize(), page.getNumberOfElements(),
-			"Expected the size of the returned page to equal what was requested");
+		assertThat(pageRequest.getPageSize())
+			.as("Expected the size of the returned page to equal what was requested")
+			.isEqualTo(page.getNumberOfElements());
 	}
+
+	@TestConfiguration
+	public static class TestConfig {
+		@Bean
+		@ConfigurationProperties("spring.datasource")
+		public DataSource dataSource() {
+			return new DriverManagerDataSource();
+		}
+
+		@Bean
+		public TaskExecutionDaoFactoryBean taskExecutionDaoFactoryBean(DataSource dataSource) {
+			return new TaskExecutionDaoFactoryBean(dataSource);
+		}
+
+		@Bean
+		public TaskExecutionDao taskExecutionDao(TaskExecutionDaoFactoryBean taskExecutionDaoFactoryBean) throws Exception {
+			return taskExecutionDaoFactoryBean.getObject();
+		}
+	}
+
 }
